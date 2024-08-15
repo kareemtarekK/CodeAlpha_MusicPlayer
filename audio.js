6167c93c84e971e17a0f99e4421ac561b1f61892
kareemtarek <119542748+kareemtarekK@users.noreply.github.com>
2024-08-15 18:33:54 +0300

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,33 @@
+const playlist = [
+  {
+    souraName: "الاخلاص",
+    souraPath: "audios/الاخلاص.mp3",
+    souraImage: "images/Surat_Al-Ikhlas.png",
+    souraReader: "Maher Al Mueaqly",
+  },
+  {
+    souraName: "الاعلى",
+    souraPath: "audios/الاعلى.mp3",
+    souraImage: "images/Surah_Al-Ala.png",
+    souraReader: "Ahmed Al-Ajami",
+  },
+  {
+    souraName: "الغاشية",
+    souraPath: "audios/الغاشية.mp3",
+    souraImage: "images/Surah_Al-Ghashiyah.png",
+    souraReader: "Maher Al Mueaqly",
+  },
+  {
+    souraName: "القارعة",
+    souraPath: "audios/القارعة.mp3",
+    souraImage: "images/Surah_Al-Qaria.png",
+    souraReader: "Yasser Al-Dosari",
+  },
+  {
+    souraName: "الكافرون",
+    souraPath: "audios/الكافرون.mp3",
+    souraImage: "images/Surah_Al-Kafirun.png",
+    souraReader: "Nasser Al-Qatami",
+  }
+];
+export default playlist;
