a94a8a65a1045531826013e1ed2b8e9ee7927c99
kareemtarek <119542748+kareemtarekK@users.noreply.github.com>
2024-08-15 18:46:34 +0300

    Update audio.js

--- audio.js
+++ audio.js
@@ -6,28 +6,28 @@ const playlist = [
     souraReader: "Maher Al Mueaqly",
   },
   {
-    souraName: "الاعلى",
-    souraPath: "audios/الاعلى.mp3",
+    souraName: "العصر",
+    souraPath: "audios/العصر.mp3",
     souraImage: "images/Surah_Al-Ala.png",
-    souraReader: "Ahmed Al-Ajami",
-  },
-  {
-    souraName: "الغاشية",
-    souraPath: "audios/الغاشية.mp3",
-    souraImage: "images/Surah_Al-Ghashiyah.png",
-    souraReader: "Maher Al Mueaqly",
-  },
-  {
-    souraName: "القارعة",
-    souraPath: "audios/القارعة.mp3",
-    souraImage: "images/Surah_Al-Qaria.png",
     souraReader: "Yasser Al-Dosari",
   },
   {
-    souraName: "الكافرون",
-    souraPath: "audios/الكافرون.mp3",
-    souraImage: "images/Surah_Al-Kafirun.png",
+    souraName: "الكوثر",
+    souraPath: "audios/الكوثر.mp3",
+    souraImage: "images/Surah_Al-Ghashiyah.png",
+    souraReader: "Nasser Al-Qatamiy",
+  },
+  {
+    souraName: "النصر",
+    souraPath: "audios/النصر.mp3",
+    souraImage: "images/Surah_Al-Qaria.png",
     souraReader: "Nasser Al-Qatami",
+  },
+  {
+    souraName: "قريش",
+    souraPath: "audios/قريش.mp3",
+    souraImage: "images/Surah_Al-Kafirun.png",
+    souraReader: "Yasser Al-Dosari",
   }
 ];
 export default playlist;
