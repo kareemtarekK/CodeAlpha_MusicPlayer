4ed232716125135eef9e7d2554df44cdce4e7401
kareemtarek <119542748+kareemtarekK@users.noreply.github.com>
2024-08-15 19:08:13 +0300

    Update audio.js

--- audio.js
+++ audio.js
@@ -2,31 +2,31 @@ const playlist = [
   {
     souraName: "الاخلاص",
     souraPath: "audios/الاخلاص.mp3",
-    souraImage: "images/Surat_Al-Ikhlas.png",
+    souraImage: "images/img.png",
     souraReader: "Maher Al Mueaqly",
   },
   {
     souraName: "العصر",
     souraPath: "audios/العصر.mp3",
-    souraImage: "images/Surah_Al-Asr.png",
+    souraImage: "images/img.png",
     souraReader: "Yasser Al-Dosari",
   },
   {
     souraName: "الكوثر",
     souraPath: "audios/الكوثر.mp3",
-    souraImage: "images/Surah_Al-Kawthar.png",
+    souraImage: "images/img.png",
     souraReader: "Nasser Al-Qatamiy",
   },
   {
     souraName: "النصر",
     souraPath: "audios/النصر.mp3",
-    souraImage: "images/Surah_Al-Nasr.png",
+    souraImage: "images/img.png",
     souraReader: "Nasser Al-Qatami",
   },
   {
     souraName: "قريش",
     souraPath: "audios/قريش.mp3",
-    souraImage: "images/Surah_Quraysh.png",
+    souraImage: "images/img.png",
     souraReader: "Yasser Al-Dosari",
   }
 ];
